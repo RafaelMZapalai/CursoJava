7f7dea50d0e4304b96481250498989a2e7a0f077
Rafael Zapalai <10880893990@edu.udesc.br>
2024-09-20 10:12:18 -0300

    Primeiro programa

---
+++ src/Fundamentos/primeiroPrograma.java
@@ -0,0 +1,14 @@
+package Fundamentos;
+/**
+* Esta classe representa a primeira parte do curso de programaçao em java
+* @author Rafael Zapalai <rafaellzapalai@hotmail.com>
+* @since JDK 21.0
+*/
+public class primeiroPrograma {
+    public static void main(String[] args) {
+        //uma sentença de codigo termina em;
+        System.out.println("primeiro programa #01!");
+        System.out.println("primeiro programa #02!");
+        System.out.println("Fim!");
+    }
+}
